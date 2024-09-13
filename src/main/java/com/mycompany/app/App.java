974f6fe488fe378d70b0d8c6c1f2d49e486b04b4
joannaz27 <joannaz27@nycstudents.net>
2024-09-13 09:25:49 -0400

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,12 +1,20 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
-
-package com.mycompany.app;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello Remote World!");
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welome to my calculator!");
+        System.out.println("Enter your first number.");
+        Scanner s = new Scanner(System.in);
+        int first = s.nextInt();
+        System.out.println("What operation would you like to do?");
+        String operation = s.next();
+        System.out.println("Enter your second number.");
+        int second = s.nextInt();
+        if(operation.equals("+"))
+        {
+            System.out.println("Answer: " + (first + second));
+        }
+        s.close();
     }
 }
